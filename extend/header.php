61e34baf5c66230ec809616e558bb8fcb518c4b8
alexanderagreda <alexanderagreda@gmail.com>
2020-01-29 22:23:27 -0500

    add directory

---
+++ extend/header.php
@@ -0,0 +1,24 @@
+<?php include 'conexion/conexion.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.css">
+
+    <title>Plan de desarrollo 2020 - 2023</title>
+  </head>
+  <body style="background:#e9f3eb;">
+    <main>
+      <div class="container mt-5 mb-5">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h3 class="text-success mb-3 font-weight-bold">Construyamos Juntos el Plan de Desarrollo del Cauca <br />2020 – 2023</h3>
+            <div class="text-center mb-3">
+              <img src="img/logo.png" alt="" width="150px">
+            </div>                    
+          </div><!--end col1-->
